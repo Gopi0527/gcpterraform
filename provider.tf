e86cda20476778cf161061e5ea7d4a1ce1e08f7e
Gopi0527 <gopikrishna0527@gmail.com>
2024-04-15 09:28:54 +0530

    added changes

--- provider.tf
+++ provider.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "google" {
-   credentials = file(Downloads/acoustic-cargo-416714-8da8162748ae.json)
+   credentials = file("/home/gopikrishnaqt/gcpterraform/acoustic-cargo-416714-8da8162748ae.json")
   project     = "My First Project"
   region      = "us-central1"
 }
