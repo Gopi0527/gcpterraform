a33024787084025d19e08eab05452f4861e2ec85
Gopi0527 <gopikrishna0527@gmail.com>
2024-04-15 09:59:17 +0530

    added changes

--- main.tf
+++ main.tf
@@ -1,9 +1,17 @@
 resource "google_compute_network" "vpc-network" {
-name = "terraform-network"
+name = "first-network"
+auto_create_subnetworks = false
 }
 resource "google_compute_subnetwork" "public-subnetwork" {
-name = "terraform-subnetwork"
+name = "first-subnetwork1"
 ip_cidr_range = "10.2.0.0/16"
 region = "us-central1"
 network = google_compute_network.vpc-network.name
 }
+resource "google_compute_subnetwork" "public-subnetwork" {
+name = "first-subnetwork1"
+ip_cidr_range = "10.3.0.0/16"
+region = "us-central1"
+network = google_compute_network.vpc-network.name
+}
+ 
