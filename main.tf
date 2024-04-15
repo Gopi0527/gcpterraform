7292556bf1f3b50b1bd9f8c1cfc4907a200e9b7d
Gopi0527 <gopikrishna0527@gmail.com>
2024-04-15 09:32:29 +0530

    added changes

--- main.tf
+++ main.tf
@@ -1,13 +1,9 @@
-resource "google_compute_network" "first-vpc" {
-  project                 = "My First Project"
-  name                    = "first-vpc"
-  auto_create_subnetworks = false
- 
+resource "google_compute_network" "vpc_network" {
+name = "terraform-network"
 }
-
-resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" {
-  name          = "first-subnet"
-  network       = google_compute_network.first-vpc.id
-  ip_cidr_range = "10.0.0.0/16"
-  region        = "us-central1"
+resource "google_compute_subnetwork" "public-subnetwork" {
+name = "terraform-subnetwork"
+ip_cidr_range = "10.2.0.0/16"
+region = "us-central1"
+network = google_compute_network.vpc_network.name
 }
