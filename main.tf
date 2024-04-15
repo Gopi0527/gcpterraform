9cf01f2f261eed16d23252b1265bfa909c39a71b
Gopi0527 <gopikrishna0527@gmail.com>
2024-04-15 09:08:30 +0530

    added changes

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-resource "google_compute_network" "first_vpc" {
+resource "google_compute_network" "first-vpc" {
   project                 = "My First Project"
-  name                    = "first_vpc"
+  name                    = "first-vpc"
   auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" {
   name          = "first-subnet"
-  network = google_compute_network.first_vpc
+  network = google_compute_network.first-vpc
   ip_cidr_range = "10.0.0.0/16"
   region        = "us-central1"
 }
